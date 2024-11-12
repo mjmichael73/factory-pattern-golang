10f09517d31a35cd80709d044ab15796db8a8b33
Mojtaba Michael <mojimich2015@gmail.com>
2024-11-12 12:52:52 -0800

    Initial commit

---
+++ ak47.go
@@ -0,0 +1,15 @@
+package main
+
+// Concrete Product
+type Ak47 struct {
+	Gun
+}
+
+func newAk47() IGun {
+	return &Ak47{
+		Gun: Gun{
+			name: "AK47 gun",
+			power: 4,
+		},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mjmichael73/factory-pattern-golang
+
+go 1.23.2
